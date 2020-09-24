89b0ceae02da85121aeb8ed74263d769433d80fe
MohammadAdnanAldaas <71332210+MohammadAdnanAldaas@users.noreply.github.com>
2020-09-24 20:34:35 +0200

    spatie tussen Hello+Name

--- helloWordConsle/Program.cs
+++ helloWordConsle/Program.cs
@@ -10,6 +10,16 @@ namespace helloWordConsle
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello Class");
+            Console.Write("what is your name?");
+            var filledin = Console.ReadLine();
+            Console.WriteLine($"hello {filledin}");
+            Console.ReadLine();
+
+         
+
+
         }
     }
 }
